addd52648a7ef57a7ec99a73ea15207ec0349ac5
Vael Victus <vael@vaelvictus.com>
2021-11-03 15:12:07 -0400

    poosh

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-Soon.
+<link rel="stylesheet" type="text/css" href="css/style.css?modified=<?=time()?>" />
+
+<span class='text-gray-800 text-2xl m-2'>Soon.</span>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    purge: [
-        './*.html'
-    ],
+    purge: {
+        enabled: true,
+        content: ['./*.php'],
+    },
+    // purge: [
+    //     './index.php'
+    // ],
     darkMode: false, // or 'media' or 'class'
     theme: {
       extend: {},
