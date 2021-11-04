56b20bc7230d582702e1935ce1148864cf49000b
Vael Victus <vael@vaelvictus.com>
2021-11-03 23:06:02 -0400

    Vite, tailwind, favicon, started styling.

--- index.php
+++ index.php
@@ -4,12 +4,17 @@
     <meta charset="UTF-8" />
     <link rel="icon" type="image/svg+xml" href="favicon.svg" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Vite App</title>
+    <title>Vael Victus</title>
   </head>
-  <body>
-    <div id="app"></div>
-    <script type="module" src="main.js"></script>
 
-    <span class='text-gray-800 text-2xl m-2'>Soon.</span>
+  <body>
+    <script type="module" src="http://localhost:1337/main.js"></script>
+
+    <div class='grid place-content-center w-screen h-screen bg-gray-50'>
+        <div class='bg-gray-400 w-1/2 h-1/2'>
+            <span class='text-gray-900 text-2xl m-5'>Soon.</span>
+        </div>
+    </div>
+    
   </body>
 </html>
