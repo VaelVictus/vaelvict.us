570d1fe8b1574657174d3eb3b7272b528b5e0ccf
Vael Victus <vael@vaelvictus.com>
2021-11-06 23:40:17 -0400

    getting started on the actual content

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Vael Victus</title>
 
-    <script type="module" crossorigin src="dist/<?=$manifest['index.html']['file']?>"></script>
-    <link rel="stylesheet" href="dist/<?=$manifest['index.html']['css'][0]?>">
+    <!-- <script type="module" crossorigin src="dist/<?=$manifest['index.html']['file']?>"></script>
+    <link rel="stylesheet" href="dist/<?=$manifest['index.html']['css'][0]?>"> -->
 
-    <!-- <script type="module" src="http://localhost:1337/main.js"></script> -->
+    <script type="module" src="http://localhost:1337/main.js"></script>
   </head>
 
   <body>
@@ -26,16 +26,17 @@
 
     <div class="grid h-screen place-items-center bg-cover" style='background-image: url("img/bg.png");'>
         <div class="w-full flex flex-wrap shadow-md 
-                    md:w-9/12 xl:w-6/12
+                    md:w-9/12 max-w-3xl
                   bg-white bg-opacity-70">
 
             <div class="w-full text-black">
-                --- Connect ---
+                <h3>--- Connect ---</h3>
             </div>
 
-            <div class='flex border-b-2'>
-                <div class="w-9/12 bg-white">
-                    hi im vael
+            <div class='flex flex-wrap border-b-2'>
+                <div class="w-48 flex flex-grow bg-white">
+                    <img class='w-1/4' src='img/brands/discord.svg'>
+                    <div class='w-3/4'>Heyo!</div>
                 </div>
             </div>
             
