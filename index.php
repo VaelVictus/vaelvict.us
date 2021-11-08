9bddcf7cea11c00671cead726f606d6000c40071
Vael Victus <vael@vaelvictus.com>
2021-11-07 22:52:31 -0500

    Messing around with grid, regretting choosing postcss

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     $manifest = json_decode(file_get_contents('dist/manifest.json'), true);
 
     // ! Cards with text, opting for just icons for now.
-    /*    <div class="w-60 flex flex-grow mr-2 align-items bg-white rounded">
-            <img class='w-1/4 mr-2' src='img/brands/discord.svg'>
-            <div class='card_txt w-3/4 text-base' style='color: #5865F2;'>
+    /*    <div class="flex flex-grow mr-2 align-items bg-white rounded">
+            <img class='w-1/5 mr-2' src='img/brands/discord.svg'>
+            <div class='card_txt w-4/5 text-base' style='color: #5865F2;'>
                 Vael Victus#0001
             </div>
         </div> */
@@ -34,6 +34,13 @@
             display: flex;
             flex-direction: column;
         }
+
+        <? # Not bothering with PostCSS's @media parsing ?>
+        @media only screen and (min-width: 768px) {
+            .grid-bubbles {
+                grid-template-columns: repeat(auto-fill, 32%);
+            }
+        }
     </style>
 
     <div class="grid h-screen place-items-center bg-cover" style='background-image: url("img/bg.png");'>
@@ -43,56 +50,57 @@
 
             <div class="w-full text-black">
                 <h1>--- Connect ---</h1>
-                Links open in new window
+                <div class='text-gray-700 italic'>Links open in new window</div>
             </div>
 
-            <div class='flex flex-wrap border-b-2'>
+            <div class='border-b-2 w-full grid grid-bubbles px-2'>
 
                 <!-- TODO: see if I can get away with space-evenly -->
-                <div class='w-60 flex m-2 align-items bg-white rounded-lg' target='_blank'>
-                    <img class='w-1/4' src='img/brands/discord.svg'>
-                    <div class='card_txt w-3/4 text-base pl-2' style='color: #5865F2;'>
+                <div class='flex align-items bg-white rounded-lg' target='_blank'>
+                    <img class='w-1/5' src='img/brands/discord.svg'>
+                    <div class='card_txt w-4/5 text-base pl-2' style='color: #5865F2;'>
                         Vael Victus#0001
                     </div>
                 </div>
                 
-                <a class='w-60 flex m-2 align-items bg-white rounded-lg' href='https://www.twitch.tv/vaelvictus' target='_blank' style='color: #6441a4;'>
-                    <img class='w-1/4' src='img/brands/twitch.svg'>
-                    <div class='card_txt w-3/4 text-base pl-2'>
+                <? /* Mobile: most people have mailto: functionality */ ?>
+                <a class='flex align-items bg-white rounded-lg text-black md:hidden' href='mailto:vael@tinydark.com' target='_blank'>
+                    <img class='w-1/5' src='img/brands/email.svg'>
+                    <div class='card_txt w-4/5 text-base pl-2'>
+                        vael@tinydark.com
+                    </div>
+                </a>
+
+                <? /* Plaintext for desktop */ ?>
+                <div class='hidden align-items bg-white rounded-lg text-black md:flex' target='_blank'>
+                    <img class='w-1/5' src='img/brands/email.svg'>
+                    <div class='card_txt w-4/5 text-base pl-2'>
+                        vael@tinydark.com
+                    </div>
+                </div>
+                
+                <a class='flex align-items bg-white rounded-lg' href='https://www.twitch.tv/vaelvictus' target='_blank' style='color: #6441a4;'>
+                    <img class='w-1/5' src='img/brands/twitch.svg'>
+                    <div class='card_txt w-4/5 text-base pl-2'>
                         Vael Victus
                     </div>
                 </a>
 
-                <a class='w-60 flex m-2 align-items bg-white rounded-lg text-black hover:shadow-md transition-shadow' href='https://steamcommunity.com/id/vaelvictus/' target='_blank'>
-                    <img class='w-1/4' src='img/brands/steam.svg'>
-                    <div class='card_txt w-3/4 text-base pl-2'>
+                <a class='flex align-items bg-white rounded-lg text-black hover:shadow-md transition-shadow' href='https://steamcommunity.com/id/vaelvictus/' target='_blank'>
+                    <img class='w-1/5' src='img/brands/steam.svg'>
+                    <div class='card_txt w-4/5 text-base pl-2'>
                         Vael Victus
                     </div>
                 </a>
                 
-                <a class='w-60 flex m-2 align-items bg-white rounded-lg text-black' href='https://github.com/VaelVictus' target='_blank'>
-                    <img class='w-1/4' src='img/brands/github.svg'>
-                    <div class='card_txt w-3/4 text-base pl-2'>
+                <a class='flex align-items bg-white rounded-lg text-black' href='https://github.com/VaelVictus' target='_blank'>
+                    <img class='w-1/5' src='img/brands/github.svg'>
+                    <div class='card_txt w-4/5 text-base pl-2'>
                         Vael Victus
                     </div>
                 </a>
 
-                <!-- 
-                    <img class='w-1/6 mx-2' src='img/brands/discord.svg'>
-                    <a href='https://steamcommunity.com/id/vaelvictus/' target='_blank'>
-                    <img class='w-1/6 mx-2' src='img/brands/steam.svg'>
-                </a>
-
-                <a href='https://www.twitch.tv/vaelvictus' target='_blank'>
-                    <img class='w-1/6 mx-2' src='img/brands/twitch.svg'>
-                </a>
-
-                <a href='https://github.com/VaelVictus' target='_blank'>
-                    <img class='w-1/6 mx-2' src='img/brands/github.svg'>
-                </a> -->
-
             </div>
-            
         </div>
     </div>
     
