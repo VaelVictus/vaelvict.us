46d5823dc2d11ba7de1912e452fd3846592bebbe
Vael Victus <vael@vaelvictus.com>
2021-11-10 17:55:31 -0500

    Iteration 2

--- inc/helpers.php
+++ inc/helpers.php
@@ -6,22 +6,29 @@
             // find the difference between current value for the date, and input date
             $yeardiff = date("Y") - $birthyear;
             $monthdiff = date("n") - $birthmonth;
-            $daydiff = date("j") - $birthday;
             
             // it will be negative if the date has not occured this year
             if ($monthdiff < 0)
             $yeardiff--;
             
-            // while the kids are age 5 or younger, display 1/2
+            // while the kids are age 4 or younger, display 1/2
             $half = '';
-            if ($yeardiff <= 5) {
+            
+            if ($yeardiff <= 4) {
                 $mos = months($birthdate);
                 if (($mos - ($yeardiff * 12)) > 6) {
                     $half = '&#189;';
                 }
             }
+
+            $age = $yeardiff.$half. ' years';
+
+            // Just give months for our cute little babies!
+            if ($yeardiff == 0) {
+                $age = months($birthdate) . ' months';
+            }
             
-        return "<span title='$birthdate'>$yeardiff$half</span>";
+        return "<span title='$birthdate'>$age</span>";
     }
 
     function months($date)
--- index.php
+++ index.php
@@ -34,10 +34,10 @@
 
     <title>Vael Victus</title>
 
-    <!-- <script type="module" crossorigin src="dist/<?=$manifest['index.html']['file']?>"></script>
-    <link rel="stylesheet" href="dist/<?=$manifest['index.html']['css'][0]?>"> -->
+    <script type="module" crossorigin src="dist/<?=$manifest['index.html']['file']?>"></script>
+    <link rel="stylesheet" href="dist/<?=$manifest['index.html']['css'][0]?>">
 
-    <script type="module" src="http://localhost:1337/main.js"></script>
+    <!-- <script type="module" src="http://localhost:1337/main.js"></script> -->
   </head>
 
   <body>
@@ -47,126 +47,151 @@
             display: flex;
             flex-direction: column;
         }
-
-        <? # Not bothering with PostCSS's @media parsing ?>
-
-        @media only screen and (min-width: 420px) {
-            .grid-bubbles {
-                grid-template-columns: repeat(auto-fill, 50%);
-            }
-        }
-
-        @media only screen and (min-width: 768px) {
-            .grid-bubbles {
-                grid-template-columns: repeat(auto-fill, 33.3%);
-            }
-        }
     </style>
 
     <main class="grid text-base h-screen place-items-center bg-cover" style='background-image: url("img/bg-min.png");'>
-        <div class="w-full md:w-9/12 max-w-3xl shadow-xl
-                  bg-white bg-opacity-30">
+        <div class="w-full md:w-9/12 max-w-4xl shadow-xl
+                    flex flex-wrap 
+                  bg-white">
 
-            <div class="w-full p-2 shadow-sm  text-black bg-white bg-opacity-40">
-                <h1 class='m-0 text-xl'>About Me</h1>
-            </div>
-            
-            <div class='w-full p-2 py-3'>
-                <div class='text-base'>Hi, I'm Vael Victus. I run <a href='https://tinydark.com'>tinydark</a>, an ethics-focused indie game microstudio. I'm married to the incomparable <a href='https://500px.com/p/evelynvictus?view=galleries'>Evelyn Victus</a>. We have two kids: Abel, <?=$abel?>, and Violet, <?=$violet?>.</div>
-            </div>
-            
-            <div class="w-full p-2 shadow-sm  text-black bg-white bg-opacity-40">
-                <h1 class='m-0 text-xl'>My Work</h1>
-            </div>
+            <h1 class='w-full p-2 m-0 text-center bg-black text-white text-2xl'>
+                Vael Victus
+            </h1>
 
-            <div class='w-full p-2 py-3'>
-                <ul>
-                    <li>Bullets? Eh.</li>
-                    <li>Bullets? Eh.</li>
-                </ul>
-            </div>
-
-            <div class="w-full p-2 shadow-sm  text-black bg-white bg-opacity-40">
-                <h1 class='m-0 text-xl'>Connect</h1>
-                <!-- <div class='text-gray-800 pt-1'>Links open in new window</div> -->
-            </div>
-
-            <div class='w-full p-2 py-3 grid grid-bubbles'>
-                
-                <? /* Email.  Mobile: most people have mailto: functionality */ ?>
-                <a class='flex align-items no-underline bg-white  text-black md:hidden hover:shadow-md transition-shadow' href='mailto:vael@tinydark.com' target='_blank'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/mail.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        <span class='underline'>vael@tinydark.com</span>
-                        <div class='text-gray-600 text-xs mt-1'>tap to mail</div>
+            <div class='w-full md:w-3/4'>
+                <div>
+                    <div class="w-full p-2 shadow-xs  text-black" style="background-color: rgba(255, 160, 11, 1)">
+                        <h2 class='m-0 text-xl'>About Me</h2>
                     </div>
-                </a>
-
-                <? /* Email. Plaintext for desktop */ ?>
-                <div class='hidden align-items bg-white  text-black md:flex hover:shadow-md transition-shadow cursor-pointer' onClick="copyToClipboard('vael@tinydark.com')" target='_blank'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/mail.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        vael@tinydark.com
-                        <div class='text-gray-600 text-xs mt-1' id='click2copy'>click to copy</div>
-                    </div>
-                </div>
-
-                
-                <? /* Discord */ ?>
-                <div class='flex align-items no-underline bg-white  hover:shadow-md transition-shadow' target='_blank'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/discord.svg'>
-                    <div class='w-4/5 card_txt text-base pl-2' style='color: #5865F2;'>
-                        Vael Victus#0001
-                        <div class='text-xs mt-1'>Discord</div>
+                    
+                    <div class='w-full p-2 py-3'>
+                        <div class='text-base'>
+                            I'm a web developer, game developer, writer, and father of two. I run <a href='https://tinydark.com'>tinydark</a>, an ethics-focused indie game microstudio. I spend most of my time making games and raising my kids.
+                        </div>
+                        
+                        <div class='text-base mt-2'>
+                            I live in Greenville, South Carolina. I'm married to the incomparable <a href='https://500px.com/p/evelynvictus?view=galleries'>Evelyn Victus</a>. We have two kids: Abel (<?=$abel?> old) and Violet (<?=$violet?> old).
+                        </div>
                     </div>
                 </div>
                 
-                <? /* Twitch */ ?>
-                <a class='flex align-items no-underline bg-white  hover:shadow-md transition-shadow' href='https://www.twitch.tv/vaelvictus' target='_blank' style='color: #6441a4;'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/twitch.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        <span class='underline'>Vael Victus</span>
-                        <div class='text-xs mt-1'>Twitch</div>
+                <div>
+                    <div class="w-full p-2 shadow-xs  text-black" style="background-color: rgba(255, 78, 50, 1);">
+                        <h2 class='m-0 text-xl'>My Work</h2>
                     </div>
-                </a>
 
-                <? /* Twitter */ ?>
-                <a class='flex align-items no-underline text-white hover:shadow-md transition-shadow' href='https://twitter.com/VaelVictus' target='_blank' style='background: #1DA1F2;'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/twitter.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        <span class='underline'>@VaelVictus</span>
-                        <div class='text-xs mt-1'>Twitter</div>
-                    </div>
-                </a>
+                    <div class='w-full p-2 py-3'>
+                        I publish my games under <a href='https://tinydark.com'>tinydark</a>. I adhere to a <a href='https://tinydark.com'>code of ethics</a> with my design.
 
-                <? /* Steam */ ?>
-                <a class='flex align-items no-underline  text-white hover:shadow-md transition-shadow' href='https://steamcommunity.com/id/vaelvictus/' target='_blank' style='background: #171a21;'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/steam.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        <span class='underline'>Vael Victus</span>
-                        <div class='text-xs mt-1'>Steam</div>
-                    </div>
-                </a>
-                
-                <? /* Github */ ?>
-                <a class='flex align-items no-underline text-white hover:shadow-md transition-shadow' href='https://github.com/VaelVictus' target='_blank' style='background: #111;'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/github.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        <span class='underline'>Vael Victus</span>
-                        <div class='text-xs mt-1'>GitHub</div>
-                    </div>
-                </a>
+                        <h3>Games</h3>
+                        <ul>
+                            <li><a href='https://www.theorbium.com/urpg'>URPG (alpha)</a> - Open-World Roleplaying MMO</li>
+                            <li>Black Crown (2022) - Narrative Horror Game</li>
+                            <li>Bean Grower (2018) - Casual Strategy Game</li>
+                            <li><a href='https://monbre.com/'>MonBre</a> (2010) - Monster MMORPG</li>
+                        </ul>
 
-                <? /* Stack Exchange */ ?>
-                <a class='flex align-items no-underline bg-white  text-black hover:shadow-md transition-shadow' href='https://stackexchange.com/users/262546/vael-victus' target='_blank'>
-                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/stackexchange.svg'>
-                    <div class='card_txt w-4/5 text-base pl-1'>
-                        <span class='underline'>Vael Victus</span>
-                        <div class='text-xs mt-1'>Stack Exchange</div>
-                    </div>
-                </a>
+                        <h3>Software</h3>
+                        <ul>
+                            <li>GAM3, proprietary web game engine</li>
+                            <li><a href='http://hub.tinydark.com/'>Tinydark Hub</a></li>
+                            <li><a href='http://lab.tinydark.com/'>Tinydark Lab</a>, </li>
+                        </ul>
 
+                        <h3>Writing</h3>
+                        More soon.
+                        <ul>
+                            <li>Two analytical teardowns of browser games: <a href='https://vael.tumblr.com/post/187341440887/die2nite-teardown-a-teardown-is-a-document-that'>Die2Nite</a> and <a href='https://vael.tumblr.com/post/634149707769430016/marosia-teardown-2020-final'>Marosia</a></li>
+                            <li>Blogging <a href='https://vael.tumblr.com/'>on Tumblr</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
+
+            <div class='w-full md:w-1/4'>
+                <div class="w-full p-2 shadow-xs  text-black " style="background-color: rgba(0, 212, 129, 1);">
+                    <h1 class='m-0 text-xl'>Connect</h1>
+                    <!-- <div class='text-gray-800 pt-1'>Links open in new window</div> -->
+                </div>
+
+                <div class='w-full grid grid-bubbles'>
+                    
+                    <? /* Email.  Mobile: most people have mailto: functionality */ ?>
+                    <a class='flex align-items shadow no-underline bg-white  text-black md:hidden' href='mailto:vael@tinydark.com' target='_blank'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/mail.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            <span class='underline'>vael@tinydark.com</span>
+                            <div class='text-gray-600 text-xs mt-1'>tap to mail</div>
+                        </div>
+                    </a>
+
+                    <? /* Email. Plaintext for desktop */ ?>
+                    <div class='hidden align-items shadow bg-white  text-black md:flex cursor-pointer' onClick="copyToClipboard('vael@tinydark.com')" target='_blank'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/mail.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            vael@tinydark.com
+                            <div class='text-gray-600 text-xs mt-1' id='click2copy'>click to copy</div>
+                        </div>
+                    </div>
+
+                    
+                    <? /* Discord */ ?>
+                    <div class='flex align-items shadow no-underline bg-white ' target='_blank'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/discord.svg'>
+                        <div class='w-4/5 card_txt text-base pl-2' style='color: #5865F2;'>
+                            Vael Victus#0001
+                            <div class='text-xs mt-1'>Discord</div>
+                        </div>
+                    </div>
+                    
+                    <? /* Twitch */ ?>
+                    <a class='flex align-items shadow no-underline bg-white ' href='https://www.twitch.tv/vaelvictus' target='_blank' style='color: #6441a4;'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/twitch.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            <span class='underline'>Vael Victus</span>
+                            <div class='text-xs mt-1'>Twitch</div>
+                        </div>
+                    </a>
+
+                    <? /* Twitter */ ?>
+                    <a class='flex align-items shadow no-underline text-white' href='https://twitter.com/VaelVictus' target='_blank' style='background: #1DA1F2;'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/twitter.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            <span class='underline'>@VaelVictus</span>
+                            <div class='text-xs mt-1'>Twitter</div>
+                        </div>
+                    </a>
+
+                    <? /* Steam */ ?>
+                    <a class='flex align-items shadow no-underline  text-white' href='https://steamcommunity.com/id/vaelvictus/' target='_blank' style='background: #171a21;'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/steam.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            <span class='underline'>Vael Victus</span>
+                            <div class='text-xs mt-1'>Steam</div>
+                        </div>
+                    </a>
+                    
+                    <? /* Github */ ?>
+                    <a class='flex align-items shadow no-underline text-white' href='https://github.com/VaelVictus' target='_blank' style='background: #111;'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/github.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            <span class='underline'>Vael Victus</span>
+                            <div class='text-xs mt-1'>GitHub</div>
+                        </div>
+                    </a>
+
+                    <? /* Stack Exchange */ ?>
+                    <a class='flex align-items shadow no-underline bg-white  text-black' href='https://stackexchange.com/users/262546/vael-victus' target='_blank'>
+                        <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/stackexchange.svg'>
+                        <div class='card_txt w-4/5 text-base pl-1'>
+                            <span class='underline'>Vael Victus</span>
+                            <div class='text-xs mt-1'>Stack Exchange</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+
         </div>
     </main>
     
