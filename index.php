42c23226edb76d6e264f81e0d5995ebc0b562e17
Vael Victus <vael@vaelvictus.com>
2021-11-09 20:10:54 -0500

    Socials done

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
     // ! Cards with text, opting for just icons for now.
     /*    <div class="flex flex-grow mr-2 align-items bg-white rounded">
-            <img class='w-1/6 md:w-1/5 mr-2' src='img/brands/discord.svg'>
+            <img class='w-1/6 md:w-1/5 mx-1 my-2 mr-2' src='img/brands/discord.svg'>
             <div class='card_txt w-4/5 text-base' style='color: #5865F2;'>
                 Vael Victus#0001
             </div>
@@ -47,13 +47,13 @@
 
         @media only screen and (min-width: 420px) {
             .grid-bubbles {
-                grid-template-columns: repeat(auto-fill, 47%);
+                grid-template-columns: repeat(auto-fill, 50%);
             }
         }
 
         @media only screen and (min-width: 768px) {
             .grid-bubbles {
-                grid-template-columns: repeat(auto-fill, 31%);
+                grid-template-columns: repeat(auto-fill, 33.3%);
             }
         }
     </style>
@@ -70,59 +70,75 @@
             <div class='w-full my-2 grid grid-bubbles px-2'>
                 
                 <? /* Email.  Mobile: most people have mailto: functionality */ ?>
-                <a class='flex align-items py-2 bg-white  text-black md:hidden hover:shadow-md transition-shadow' href='mailto:vael@tinydark.com' target='_blank'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/mail.svg'>
-                    <div class='card_txt w-4/5 text-base pl-2'>
-                        vael@tinydark.com
+                <a class='flex align-items no-underline bg-white  text-black md:hidden hover:shadow-md transition-shadow' href='mailto:vael@tinydark.com' target='_blank'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/mail.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>vael@tinydark.com</span>
+                        <div class='text-gray-600 text-xs mt-1'>tap to mail</div>
                     </div>
                 </a>
 
                 <? /* Email. Plaintext for desktop */ ?>
-                <div class='hidden align-items bg-white  text-black md:flex hover:shadow-md transition-shadow' target='_blank'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/mail.svg'>
-                    <div class='card_txt w-4/5 text-base pl-2'>
-                        vael@tinydark.com
+                <div class='hidden align-items bg-white  text-black md:flex hover:shadow-md transition-shadow cursor-pointer' onClick="copyToClipboard('vael@tinydark.com')" target='_blank'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/mail.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>vael@tinydark.com</span>
+                        <div class='text-gray-600 text-xs mt-1' id='click2copy'>click to copy</div>
                     </div>
                 </div>
 
                 
                 <? /* Discord */ ?>
-                <div class='flex align-items py-2 bg-white  hover:shadow-md transition-shadow' target='_blank'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/discord.svg'>
+                <div class='flex align-items no-underline bg-white  hover:shadow-md transition-shadow' target='_blank'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/discord.svg'>
                     <div class='w-4/5 card_txt text-base pl-2' style='color: #5865F2;'>
-                        Vael Victus#0001
+                        <span class='underline'>Vael Victus#0001</span>
+                        <div class='text-xs mt-1'>Discord</div>
                     </div>
                 </div>
                 
                 <? /* Twitch */ ?>
-                <a class='flex align-items py-2 bg-white  hover:shadow-md transition-shadow' href='https://www.twitch.tv/vaelvictus' target='_blank' style='color: #6441a4;'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/twitch.svg'>
-                    <div class='card_txt w-4/5 text-base pl-2'>
-                        Vael Victus
+                <a class='flex align-items no-underline bg-white  hover:shadow-md transition-shadow' href='https://www.twitch.tv/vaelvictus' target='_blank' style='color: #6441a4;'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/twitch.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>Vael Victus</span>
+                        <div class='text-xs mt-1'>Twitch</div>
+                    </div>
+                </a>
+
+                <? /* Twitter */ ?>
+                <a class='flex align-items no-underline text-white hover:shadow-md transition-shadow' href='https://twitter.com/VaelVictus' target='_blank' style='background: #1DA1F2;'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/twitter.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>@VaelVictus</span>
+                        <div class='text-xs mt-1'>Twitter</div>
                     </div>
                 </a>
 
                 <? /* Steam */ ?>
-                <a class='flex align-items py-2  text-white hover:shadow-md transition-shadow' href='https://steamcommunity.com/id/vaelvictus/' target='_blank' style='background: #171a21;'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/steam.svg'>
-                    <div class='card_txt w-4/5 text-base pl-2'>
-                        Vael Victus
+                <a class='flex align-items no-underline  text-white hover:shadow-md transition-shadow' href='https://steamcommunity.com/id/vaelvictus/' target='_blank' style='background: #171a21;'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/steam.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>Vael Victus</span>
+                        <div class='text-xs mt-1'>Steam</div>
                     </div>
                 </a>
                 
                 <? /* Github */ ?>
-                <a class='flex align-items py-2 bg-black  text-white hover:shadow-md transition-shadow' href='https://github.com/VaelVictus' target='_blank'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/github.svg'>
-                    <div class='card_txt w-4/5 text-base pl-2'>
-                        Vael Victus
+                <a class='flex align-items no-underline text-white hover:shadow-md transition-shadow' href='https://github.com/VaelVictus' target='_blank' style='background: #111;'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/github.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>Vael Victus</span>
+                        <div class='text-xs mt-1'>GitHub</div>
                     </div>
                 </a>
 
-                <? /* Github */ ?>
-                <a class='flex align-items py-2 bg-black  text-white hover:shadow-md transition-shadow' href='https://github.com/VaelVictus' target='_blank'>
-                    <img class='w-1/6 md:w-1/5' src='img/brands/github.svg'>
-                    <div class='card_txt w-4/5 text-base pl-2'>
-                        Vael Victus
+                <? /* Stack Exchange */ ?>
+                <a class='flex align-items no-underline bg-white  text-black hover:shadow-md transition-shadow' href='https://stackexchange.com/users/262546/vael-victus' target='_blank'>
+                    <img class='w-1/6 md:w-1/5 mx-1 my-2' src='img/brands/stackexchange.svg'>
+                    <div class='card_txt w-4/5 text-base pl-1'>
+                        <span class='underline'>Vael Victus</span>
+                        <div class='text-xs mt-1'>Stack Exchange</div>
                     </div>
                 </a>
 
--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import './css/style.css'
+
+window.copyToClipboard = function(message) {
+  var textArea = document.createElement("textarea");
+  textArea.value = message;
+  textArea.style.opacity = "0"; 
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+      document.execCommand('copy');
+
+      // *yawn*
+      document.getElementById('click2copy').innerHTML = '<b style="color: #228C22">✔ copied</b>';
+      setTimeout(() => {
+        document.getElementById('click2copy').innerHTML = 'click to copy';
+      }, 3000);
+  } catch (err) {
+      console.log('Unable to copy value , error : ' + err.message);
+  }
+
+  document.body.removeChild(textArea);
+}
