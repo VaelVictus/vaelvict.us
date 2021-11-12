4fa0973140ecea2ae757b6fa73ceed462fe41678
Vael Victus <vael@vaelvictus.com>
2021-11-12 14:18:31 -0500

    easing

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ anime.timeline({loop: false})
     translateX: [40,0],
     translateZ: 0,
     opacity: [0,1],
-    easing: "easeOutExpo",
-    duration: 3500,
-    delay: (el, i) => 100 + 30 * i
+    easing: "easeOutQuart",
+    duration: 5000,
+    delay: (el, i) => 150 + 30 * i
   });
 
 // ! HELPERS
